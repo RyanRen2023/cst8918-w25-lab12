1b89a8cf5ab6a2561cc2fe64cc4e0386603aa243
RyanRen2023 <ren00055@algonquinlive.com>
2025-07-29 21:31:19 -0400

    fix tflint check

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "app_rg" {
-  name     = "ren00055-a12-rg"
-  location = "canadacentral"
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
+
