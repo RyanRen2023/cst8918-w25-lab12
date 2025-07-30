8f2f533e10069b5d0a967bddb4e1f416614f0796
RyanRen2023 <ren00055@algonquinlive.com>
2025-07-29 22:25:47 -0400

    change storage name

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "app_rg" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.resource_prefix}-storage-account"
+  name                     = "ren00055a12storageaccount"
   resource_group_name      = azurerm_resource_group.app_rg.name
   location                 = azurerm_resource_group.app_rg.location
   account_tier             = "Standard"
